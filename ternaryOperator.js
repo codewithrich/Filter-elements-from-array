73d51039f9040d3e8314cd6e459e28f500ec80d5
Richardson <104580727+codewithrich@users.noreply.github.com>
2023-07-10 01:07:12 +0530

    Create ternaryOperator.js

---
+++ ternaryOperator.js
@@ -0,0 +1,10 @@
+//ternary operators
+
+function OddEven(num){
+    return (num%2==0 ? "It is even" : "it is odd");
+    
+    //syntax:
+    // (ExpressionORcondition ? If true : If False);
+}
+
+console.log(OddEven(6))
